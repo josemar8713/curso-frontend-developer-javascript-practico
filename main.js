6f15b49b174b3e503fd0c19f1edd0468582ebc1e
jose martin <josemar8713@gmail.com>
2023-11-22 16:04:07 -0600

    implementacion menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const navamail =document.querySelector (".navbar-email");
 const desktopmenu=document.querySelector(".desktop-menu");
 
+const btnhamburguesa=document.querySelector(".menu")
+const menumobile=document.querySelector (".mobile-menu")
+
 navamail.addEventListener("click",toggleCorreoclick);
+
+btnhamburguesa.addEventListener("click",toggleclicham)
+
+function toggleclicham(){
+menumobile.classList.toggle("inactive")
+
+}
 
 function toggleCorreoclick(){
 desktopmenu.classList.toggle("inactive")
 }
- console.log("js funcionando");
+ 
