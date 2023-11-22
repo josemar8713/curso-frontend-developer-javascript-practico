49d0c8e3df40a0633981fb17126154997dad4dcf
jose martin <josemar8713@gmail.com>
2023-11-22 15:19:34 -0600

    primeros cambios

---
+++ main.js
@@ -0,0 +1,9 @@
+const navamail =document.querySelector (".navbar-email");
+const desktopmenu=document.querySelector(".desktop-menu");
+
+navamail.addEventListener("click",toggleCorreoclick);
+
+function toggleCorreoclick(){
+desktopmenu.classList.toggle("inactive")
+}
+ console.log("js funcionando");
