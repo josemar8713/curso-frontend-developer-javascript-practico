e4e239c1b0f9027af87f93dcb7630d0e3bbcd190
jose martin <josemar8713@gmail.com>
2023-11-22 17:46:51 -0600

    agregue inactive a funciones

--- main.js
+++ main.js
@@ -4,16 +4,28 @@ const desktopmenu=document.querySelector(".desktop-menu");
 const btnhamburguesa=document.querySelector(".menu")
 const menumobile=document.querySelector (".mobile-menu")
 
+const btncarrito =document.querySelector(".navbar-shopping-cart") 
+const aside =document.querySelector(".product-detail")
+
 navamail.addEventListener("click",toggleCorreoclick);
 
-btnhamburguesa.addEventListener("click",toggleclicham)
+btnhamburguesa.addEventListener("click",toggleclicham);
+
+btncarrito.addEventListener("click",toggleclickproductos);
 
 function toggleclicham(){
 menumobile.classList.toggle("inactive")
+aside.classList.add("inactive")
 
 }
 
 function toggleCorreoclick(){
 desktopmenu.classList.toggle("inactive")
+aside.classList.add("inactive")
+}
+
+function toggleclickproductos(){
+    aside.classList.toggle("inactive")
+    menumobile.classList.add("inactive")
 }
  
